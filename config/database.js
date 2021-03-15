43fa617db13f1a4fccd5b4619c5e573c18a6a25a
devbaraus <me@baraus.dev>
2021-03-15 10:38:26 -0300

    Update HEROKU

--- config/database.js
+++ config/database.js
@@ -1,15 +1,27 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+const parseDBUrl = require('parse-database-url')
+
+
+module.exports = ({ env }) => {
+  const { host, database, user, password, driver, port } = parseDBUrl(env('DATABASE_URL'))
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: driver,
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { "rejectUnauthorized": false }
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  }
+};
